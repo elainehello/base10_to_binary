67bb5d75eebf856e088c5e80a12853054e239ae9
elaine <195331169+elainehello@users.noreply.github.com>
2025-08-21 18:08:03 +0200

    cast

--- stack.c
+++ stack.c
@@ -2,7 +2,7 @@
 
 int binary_push(t_binary *bin, int bit)
 {
-    if (bin->size >= MAX_BITS)
+    if (bin->size >= (int)MAX_BITS) // if true (overflow)
         return (EXIT_FAILURE);
     bin->bits[bin->size++] = bit;
     return (EXIT_SUCCESS);
