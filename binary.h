32829eeb8e269ae904398844ae080ef94ac5f2c0
elaine <195331169+elainehello@users.noreply.github.com>
2025-08-21 16:50:04 +0200

    data structure

---
+++ binary.h
@@ -0,0 +1,19 @@
+#ifndef BINARY_H
+#define BINARY_H
+
+#include <limits.h>
+#include <stdlib.h>
+
+// Upper bound for binary representation of an int
+#define MAX_BITS (CHAR_BIT * sizeof(int))
+
+// Data structure for binary numbers
+typedef struct s_binary {
+    int bits[MAX_BITS]; // Array holding binary digits (each 0 or 1)
+    int size;
+} t_binary;
+
+int binary_push(t_binary *bin, int bit);
+int binary_pop(t_binary *bin, int bit);
+
+#endif
---
+++ stack.c
@@ -0,0 +1,8 @@
+#include "binary.h"
+
+int binary_push(t_binary *bin, int bit)
+{
+    if (bin->size >= MAX_BITS) // if true (overflow)
+        return (EXIT_FAILURE);
+    return (EXIT_SUCCESS);
+}
