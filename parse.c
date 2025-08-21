924b45440f4959930fbfa4c05690714340f974e9
elaine <195331169+elainehello@users.noreply.github.com>
2025-08-21 18:11:26 +0200

    upd

--- parse.c
+++ parse.c
@@ -59,32 +59,3 @@ int decimal_to_binary(int n, t_binary *bin)
     }
     return (EXIT_SUCCESS);
 }
-
-int main(int argc, char **argv)
-{
-    int result;
-    t_binary bin;
-    int i;
-
-    if (argc <= 1)
-    {
-        ft_printf("Usage: %s <number>\n", argv[0]);
-        return (EXIT_FAILURE);
-    }
-    result = atoi_ascii(argv[1]);
-    ft_printf("\n%d\n", result);
-    ft_printf("decimal to binary expression:\n");
-    bin.size = 0;
-    i = 0;
-    if (decimal_to_binary(result, &bin) == EXIT_SUCCESS)
-    {
-        i = bin.size - 1;
-        while (i >= 0)
-        {
-            ft_printf("%d", bin.bits[i]);
-            i--;
-        }
-        ft_printf("\n");
-    }
-    return (0);
-}
